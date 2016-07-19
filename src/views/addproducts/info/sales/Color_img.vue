d05ad7b79259bca4e5d3baed8f697a71bd8c9f84
小鱼 <80447081@qq.com>
2016-07-19 10:03:20 +0800

    销售属性－上传图片小修改

--- src/views/addproducts/info/sales/Color_img.vue
+++ src/views/addproducts/info/sales/Color_img.vue
@@ -1,11 +1,9 @@
 <template>
     <a @click="color_img(id)" id="Apickfiles{{id}}" href="javascript:;">
-        <img v-if="imgsrc!=''" style="max-width:70px; max-height:70px;" v-bind:src="imgIndex.qiniuurl+imgsrc" alt="" />
-
+        <img v-if="imgIndex.qiniuurl!=''&&imgsrc!=''" style="max-width:70px; max-height:70px;" v-bind:src="imgIndex.qiniuurl+imgsrc" alt="" />
         <button v-if="imgsrc==''" class="btn btn-success">
             <span>选择图片</span>
         </button>
-        <!-- <img v-if="imgsrc==''" style="width:70px; height:70px;" src="http://localhost:8080/static/img/file_242_2.45181cb.jpg" alt="" /> -->
     </a>
 </template>
 
