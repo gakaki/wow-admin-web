3c3528e1c98bf63f026839e969a5f372c6131485
小鱼 <80447081@qq.com>
2016-08-14 11:26:36 +0800

    编辑商品

--- src/views/goods/info/basic/country.vue
+++ src/views/goods/info/basic/country.vue
@@ -6,9 +6,6 @@
         <p>
             城市ID：{{originCity}}
         </p>
-        <p>
-            {{provinceList}}
-        </p>
         <label for="firstname" class="col-sm-2 control-label"><span class="text-danger">*</span>产地/国家</label>
         <div class="col-sm-4">
             <input name="originCountryId" v-bind:value="originCountryId" type="text" class="form-control" placeholder="国家">
@@ -28,6 +25,13 @@
                 <v-select :value.sync="defaultProvince" class="origin_country" :on-change="setOriginProvinceId" label="areaName" :debounce="500" placeholder="搜索省份" :options="provinceList" ></v-select>
             </div>
         </div>
+        <div class="col-sm-4">
+            <input name="originCity" v-bind:value="originCity" type="text" class="form-control" placeholder="城市">
+            <div class="input-group add-product-hide-input">
+                <span style="border-top-left-radius:4px; border-bottom-left-radius:4px;" class="input-group-addon">城市</span>
+                <v-select :on-change="setOriginCityId" :value.sync="defaultCity" class="origin_country" label="areaName" :debounce="500" placeholder="搜索城市" :options="cityList" ></v-select>
+            </div>
+        </div>
     </div>
 </template>
 <script type="text/javascript">
@@ -42,10 +46,11 @@
         data(){
             return{
                 countryList:[],
+                provinceList:[],
+                cityList:[],
                 defaultCountry:{},
                 defaultProvince:{},
-                provinceList:[],
-                cityList:[]
+                defaultCity:{},
             }
         },
         watch:{
@@ -55,19 +60,29 @@
         },
         events: {
             'countrylist': function (msg) {
-                //获取国家数据
+                //获取国家数据并设置默认值
                 this.httpGet('v1/country/queryAllCountries',{},'获取国家数据错误',(data) =>{
                     this.$set('countryList',data.data);
-                    let defaultCountry = (element) => {
-                        if (element.id==this.originCountryId) {
-                            this.$set('defaultCountry',element)
-                        }
-                    }
-                    data.data.filter(defaultCountry);
+                    this.filterObj(data.data,this.originCountryId,'defaultCountry');
                 });
             }
         },
         methods:{
+            /**
+             * 根据国家／省份／城市／三者的id查询列表里面对应的数据
+             * id用来筛选data里面哪一条数据的id是匹配的
+             * data是从服务器获取的list数据
+             * setName指定你想要set数据的变量
+             */
+            filterObj:function(data,id,setName){
+                let defaultCountry = (data) => {
+                    if (data.id==id) {
+                        this.$set(setName,data)
+                    }
+                }
+                data.filter(defaultCountry);
+            },
+
             /**
              * http请求，组件内请求数据调用该方法
              * callback是回调方法，可以在回调里面进行对应的数据绑定
@@ -83,11 +98,12 @@
                 }, (response) => {
                 });
             },
+
             /**
-             * 设置国家id的值
+             * 设置国家id的值并设置默认值
              * 非中国的国家，不用设置省份城市
              */
-            setOriginCountryId(val) {
+            setOriginCountryId:function(val) {
                 if (!val) {
                     this.$set('originCountryId','')
                     return;
@@ -100,10 +116,43 @@
                     //如果是中国，获取省份
                     this.httpGet('v1/area/subarea',{"areaId":0},'获取省份数据错误',(data)=>{
                         this.$set('provinceList',data.data.areaList);
+                        this.filterObj(data.data.areaList,this.originProvinceId,'defaultProvince');
                     });
                 }
                 this.$set('originCountryId',val.id);
             },
+
+            /**
+             * 设置省份id的值并设置默认值
+             */
+            setOriginProvinceId:function(val){
+                if (!val) {
+                    this.$set('setOriginProvinceId','')
+                    return;
+                }
+                this.$set('originCountryId',val.id)
+                this.$set('defaultCity','')
+                this.$set('originCity','');
+                this.$set('cityList',[]);
+
+                //根据省份查询对应的城市
+                this.httpGet('v1/area/subarea',{"areaId":val.id},'获取城市数据错误',(data)=>{
+                    console.log(this.originCity);
+                    this.$set('cityList',data.data.areaList);
+                    this.filterObj(data.data.areaList,this.originCity,'defaultCity');
+                });
+            },
+
+            /**
+             * 设置城市id的值并设置默认值
+             */
+            setOriginCityId:function(val){
+                if (!val) {
+                    this.$set('originCity','')
+                    return;
+                }
+                this.$set('originCity',val.id)
+            }
         }
     }
 </script>
