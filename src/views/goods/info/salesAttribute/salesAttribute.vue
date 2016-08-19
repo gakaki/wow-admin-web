5d5f8ef3cdd34fbd49fbf4cae4dfb51243d13d88
小鱼 <80447081@qq.com>
2016-08-19 12:49:09 +0800

    一键发布命令

---
+++ src/views/goods/info/salesAttribute/salesAttribute.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+<script type="text/javascript">
+    export default:{
+        data(){
+            return{
+                color:[
+                    {colorId:1,color:'#ffffff',name:'白色'},
+                    {colorId:2,color:'#ffffff',name:'灰色'},
+                    {colorId:3,color:'#ffffff',name:'黑色'},
+                    {colorId:4,color:'#ffffff',name:'红色'},
+                    {colorId:5,color:'#ffffff',name:'黄色'},
+                    {colorId:6,color:'#ffffff',name:'蓝色'},
+                    {colorId:7,color:'#ffffff',name:'绿色'},
+                    {colorId:8,color:'#ffffff',name:'紫色'},
+                    {colorId:9,color:'#ffffff',name:'棕色'},
+                    {colorId:10,color:'#ffffff',name:'原木'},
+                    {colorId:11,color:'#ffffff',name:'花色'},
+                    {colorId:12,color:'#ffffff',name:'透明'},
+                    {colorId:12,color:'#ffffff',name:'透明'},
+                ],
+                spec:[],
+            }
+        }
+    }
+</script>
